ae5891edbac6ae42885246fbab5d82e4361f4456
betrider <betrider@twosun.com>
2021-03-31 18:08:07 +0900

    수정

--- lib/utilities/extensions/context.dart
+++ lib/utilities/extensions/context.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 
 extension ContextExtension on BuildContext {
+  ///context.mediaQuery
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 }
+
 extension MediaQueryExtension on BuildContext {
+  ///context.height
   double get height => mediaQuery.size.height;
+
+  ///context.width
   double get width => mediaQuery.size.width;
+}
+
+extension NavigatorExt on BuildContext {
+
+  ///context.push(Test());
+  Future<T?> push<T>(Widget route) => Navigator.push(
+      this,
+      MaterialPageRoute(
+        builder: (_) => route,
+      ));
+
+  ///context.pop();
+  void pop<T extends Object>([T? result]) => Navigator.pop(this, result);
+
+  ///context.pushNamed('/Test');
+  Future<T?> pushNamed<T>(String routeName, {Object? arguments}) =>
+      Navigator.pushNamed<T?>(this, routeName, arguments: arguments);
+
+  ///context.canPop();
+  bool canPop() => Navigator.canPop(this);
+
+  ///context.popUntil();
+  void popUntil(RoutePredicate predicate) =>
+      Navigator.popUntil(this, predicate);
 }
--- lib/utilities/extensions/extensions.dart
+++ lib/utilities/extensions/extensions.dart
@@ -3,3 +3,7 @@ export 'string.dart';
 export 'widget.dart';
 export 'list.dart';
 export 'iterable/iterable.dart';
+
+//extension 참조
+//1.supercharged : https://pub.dev/packages/supercharged
+//2.build_context : https://github.com/pedromassango/build_context
