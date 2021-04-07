d0aa46ccefa77db75d974268f4bcf70ec4591079
김상우 <swkim@gimsang-uui-MacBookPro.local>
2021-04-07 18:30:20 +0900

    typedef 추가

--- lib/utilities/extensions/widget.dart
+++ lib/utilities/extensions/widget.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 
 extension ExtendedText on Widget {
+  //Align
 
-  ///시작 정렬
+  ///상단 시작 정렬
+  addAlignTopStart() {
+    return Align(
+      alignment: AlignmentDirectional.topStart,
+      child: this,
+    );
+  }
+
+  ///상단 중앙 정렬
+  addAlignTopCenter() {
+    return Align(
+      alignment: AlignmentDirectional.topCenter,
+      child: this,
+    );
+  }
+
+  ///상단 끝 정렬
+  addAlignTopEnd() {
+    return Align(
+      alignment: AlignmentDirectional.topEnd,
+      child: this,
+    );
+  }
+
+  ///중앙 시작 정렬
   addAlignCenterStart() {
     return Align(
       alignment: AlignmentDirectional.centerStart,
@@ -10,7 +35,15 @@ extension ExtendedText on Widget {
     );
   }
 
-  ///끝 정렬
+  ///중앙 시작 정렬
+  addAlignCenter() {
+    return Align(
+      alignment: AlignmentDirectional.center,
+      child: this,
+    );
+  }
+
+  ///중앙 끝 정렬
   addAlignCenterEnd() {
     return Align(
       alignment: AlignmentDirectional.centerEnd,
@@ -18,8 +51,34 @@ extension ExtendedText on Widget {
     );
   }
 
+  ///하단 시작 정렬
+  addAlignBottomStart() {
+    return Align(
+      alignment: AlignmentDirectional.bottomStart,
+      child: this,
+    );
+  }
+
+  ///하단 시작 정렬
+  addAlignBottomCenter() {
+    return Align(
+      alignment: AlignmentDirectional.bottomCenter,
+      child: this,
+    );
+  }
+
+  ///하단 끝 정렬
+  addAlignBottomEnd() {
+    return Align(
+      alignment: AlignmentDirectional.bottomEnd,
+      child: this,
+    );
+  }
+
+  //pading
+
   ///패딩 전체 추가
-  addPaddingAll(double value){
+  addPaddingAll(double value) {
     return Padding(
       padding: EdgeInsets.all(value),
       child: this,
@@ -27,7 +86,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 가로 추가
-  addPaddingHorizontal(double value){
+  addPaddingHorizontal(double value) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: value),
       child: this,
@@ -35,7 +94,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 세로 추가
-  addPaddingVertical(double value){
+  addPaddingVertical(double value) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: value),
       child: this,
@@ -43,7 +102,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 왼쪽 추가
-  addPaddingLeft(double value){
+  addPaddingLeft(double value) {
     return Padding(
       padding: EdgeInsets.only(left: value),
       child: this,
@@ -51,7 +110,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 오른쪽 추가
-  addPaddingRight(double value){
+  addPaddingRight(double value) {
     return Padding(
       padding: EdgeInsets.only(right: value),
       child: this,
@@ -59,7 +118,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 상단 추가
-  addPaddingTop(double value){
+  addPaddingTop(double value) {
     return Padding(
       padding: EdgeInsets.only(top: value),
       child: this,
@@ -67,7 +126,7 @@ extension ExtendedText on Widget {
   }
 
   ///패딩 하단 추가
-  addPaddingBottom(double value){
+  addPaddingBottom(double value) {
     return Padding(
       padding: EdgeInsets.only(bottom: value),
       child: this,
---
+++ lib/utilities/typedef.dart
@@ -0,0 +1,2 @@
+typedef CommonReturnFunction = dynamic Function(dynamic? value);
+typedef CommonVoidFunction = void Function();
